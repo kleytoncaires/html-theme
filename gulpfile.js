0a40fe919161083569b07198304c93fb0a0b3252
Kleyton Caires <kleytoncaires@gmail.com>
2023-09-14 00:03:58 -0300

    chore: image optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,114 +1,123 @@
-const themeName = 'theme-name';
-const gulp = require('gulp');
-const { parallel, series } = require('gulp');
+const gulp = require('gulp')
+const { parallel, series } = require('gulp')
+const plumber = require('gulp-plumber')
+const notify = require('gulp-notify')
+const fs = require('fs')
+const path = require('path')
+const webp = require('gulp-webp')
+const tinypng = require('gulp-tinypng-compress')
+const uglify = require('gulp-uglify')
+const sass = require('gulp-sass')(require('sass'))
+const concat = require('gulp-concat')
+const autoprefixer = require('gulp-autoprefixer')
+const babel = require('gulp-babel')
 
-const uglify = require('gulp-uglify');
-const sass = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const babel = require('gulp-babel');
-const wpPot = require('gulp-wp-pot');
-const sort = require('gulp-sort');
-const path = require('path');
-const fs = require('fs');
-const plumber = require('gulp-plumber');
-const notify = require('gulp-notify');
+const srcPath = 'assets/js'
+const destPath = './'
 
-// /*
-// TOP LEVEL FUNCTIONS
-//     gulp.task = Define tasks
-//     gulp.src = Point to files to use
-//     gulp.dest = Points to the folder to output
-//     gulp.watch = Watch files and folders for changes
-// */
+const scriptsToCopy = [
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+    'node_modules/jquery-mask-plugin/dist/jquery.mask.min.js',
+    'node_modules/@fancyapps/ui/dist/fancybox/fancybox.umd.js',
+    'node_modules/swiper/swiper-bundle.min.js',
+    'node_modules/jquery-validation/dist/jquery.validate.min.js',
+]
 
-// Copy Task: copy scripts from node modules to assets/scripts
+function handleErrors() {
+    return plumber({
+        errorHandler: notify.onError((error) => `Error: ${error.message}`),
+    })
+}
+
+function js() {
+    const files = fs
+        .readdirSync(srcPath)
+        .filter((file) => path.extname(file) === '.js')
+
+    const jsTasks = files.map((file) => {
+        return gulp
+            .src(path.join(srcPath, file), { sourcemaps: true })
+            .pipe(handleErrors())
+            .pipe(babel({ presets: ['@babel/preset-env'] }))
+            .pipe(concat(file))
+            .pipe(uglify())
+            .pipe(gulp.dest(destPath, { sourcemaps: '.' }))
+    })
+
+    return Promise.all(jsTasks)
+}
+
 function copyScripts(cb) {
-	const scriptsToCopy = [
-		'node_modules/jquery/dist/jquery.min.js',
-		'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
-		'node_modules/jquery-mask-plugin/dist/jquery.mask.min.js',
-		'node_modules/@fancyapps/ui/dist/fancybox/fancybox.umd.js',
-		'node_modules/swiper/swiper-bundle.min.js',
-	];
+    gulp.src(scriptsToCopy)
+        .pipe(
+            plumber({
+                errorHandler: notify.onError(
+                    'Copy Error: <%= error.message %>'
+                ),
+            })
+        )
+        .pipe(gulp.dest('assets/js/vendor'))
+        .on('end', function () {
+            console.log('Scripts copied successfully!')
+        })
 
-	gulp.src(scriptsToCopy)
-		.pipe(
-			plumber({
-				errorHandler: notify.onError(
-					'Copy Error: <%= error.message %>'
-				),
-			})
-		)
-		.pipe(gulp.dest('assets/js/vendor'))
-		.on('end', function () {
-			console.log('Scripts copied successfully!');
-		});
-
-	cb();
+    cb()
 }
 
-// JS task: concatenates and uglifies JS files to script.js
-function js(cb) {
-	const srcPath = 'assets/js';
-	const destPath = './';
-
-	const files = fs
-		.readdirSync(srcPath)
-		.filter((file) => path.extname(file) === '.js');
-
-	files.forEach((file) => {
-		gulp.src(path.join(srcPath, file), { sourcemaps: true })
-			.pipe(
-				plumber({
-					errorHandler: notify.onError(
-						'JS Error: <%= error.message %>'
-					),
-				})
-			) // Usando gulp-plumber
-			.pipe(
-				babel({
-					presets: ['@babel/preset-env'],
-				})
-			)
-			.pipe(concat(file))
-			.pipe(uglify())
-			.pipe(gulp.dest(destPath, { sourcemaps: '.' }));
-		// .pipe(notify({ message: 'JS processed successfully: ' + file }));
-	});
-
-	cb();
+function css() {
+    return gulp
+        .src('assets/css/*.scss', { sourcemaps: true })
+        .pipe(handleErrors())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(
+            autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false,
+            })
+        )
+        .pipe(gulp.dest('./', { sourcemaps: '.' }))
 }
 
-// SCSS task: compiles the style.scss file into style.css
-function css(cb) {
-	gulp.src('assets/css/*.scss', { sourcemaps: true })
-		.pipe(
-			plumber({
-				errorHandler: notify.onError('CSS Error: <%= error.message %>'),
-			})
-		) // Usando gulp-plumber
-		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-		.pipe(
-			autoprefixer({
-				browserlist: ['last 2 versions'],
-				cascade: false,
-			})
-		)
-		.pipe(gulp.dest('./', { sourcemaps: '.' }));
-	// .pipe(notify({ message: 'CSS processed successfully' }));
-
-	cb();
+function optimizeImages() {
+    return gulp
+        .src('assets/img/**/*.+(png|jpg|jpeg|gif|svg)')
+        .pipe(
+            tinypng({
+                key: process.env.TINYPNG_API_KEY,
+                sigFile: 'images/.tinypng-sigs',
+                log: true,
+            })
+        )
+        .pipe(gulp.dest('assets/img'))
 }
 
-// Watch Files
+function convertToWebP() {
+    return gulp
+        .src('assets/img/**/*.+(jpg|jpeg|png)')
+        .pipe(webp())
+        .pipe(gulp.dest('assets/img/webp'))
+}
+
+function serve(cb) {
+    browserSync.init({
+        server: {
+            baseDir: './',
+        },
+    })
+
+    gulp.watch('./*.html').on('change', browserSync.reload)
+    gulp.watch('./css/*.css').on('change', browserSync.reload)
+    gulp.watch('./js/*.js').on('change', browserSync.reload)
+
+    cb()
+}
+
 function watchFiles() {
-	gulp.watch('assets/css/**/*.scss', css);
-	gulp.watch('assets/js/*.js', js);
+    gulp.watch('assets/css/**/*.scss', css)
+    gulp.watch('assets/js/*.js', js)
 }
 
-// Default 'gulp' command with start local server and watch files for changes.
-exports.default = series(css, js, copyScripts, watchFiles);
+exports.default = series(parallel(css, js), copyScripts, serve, watchFiles)
 
-// 'gulp build' will build all assets but not run on a local server.
-exports.build = parallel(css, js);
+exports.build = parallel(css, js, optimizeImages, convertToWebP)
